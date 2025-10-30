2cd59ff10ecbac4e9797c0ec85725a10ce26a8af
benoit <scelios@outlook.fr>
2025-10-30 10:13:57 +0100

    Done for evaluate, just need to do the image for predict

--- file/Predict.py
+++ file/Predict.py
@@ -1,12 +1,34 @@
-
+#!/usr/bin/env python3
 
 import argparse
-from pathlib import Path
+import os
+import tensorflow as tf
 
-def predict(model_path, predict_image_path):
-    print("MODEL PATH", model_path)
-    print("PREDICT IMAGE PATH", predict_image_path)
-    pass
+import matplotlib
+os.environ['CUDA_VISIBLE_DEVICES'] = ''  # set to '' to disable GPU init
+# Reduce TensorFlow logs: 0=all, 1=INFO, 2=WARNING, 3=ERROR
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+matplotlib.use('TkAgg')  # select a GUI backend BEFORE importing pyplot
+import matplotlib.pyplot as plt
+from pathlib import Path
+from typing import Any
+from Utils import load_model
+
+
+def predict(model_path, predict_image_path, class_names=None):
+    img = tf.keras.utils.load_img(
+        predict_image_path, target_size=(256, 256)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model_path.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    print(
+        f"This image most likely belongs to class "
+        f"{class_names[tf.argmax(score)]} with a {100 * tf.reduce_max(score):.2f} percent confidence."
+    )
 
 
 if __name__ == "__main__":
@@ -27,9 +49,18 @@ if __name__ == "__main__":
             help=("model file, usually a .keras file")
         )
 
-        args = parser.parse_args()
+        parser.add_argument(
+            "--validation_dir",
+            type=str,
+            default="augmented_directory/validation",
+            help=("directory to look for validation subfolder")
+        )
 
-        predict(Path(args.model_path), Path(args.predict_image_path))
+        args = parser.parse_args()
+        keras_model_path = args.model_path
+        validation_dir = Path(args.validation_dir)
+        keras_model, ds_test, predict_dataset, class_names = load_model(keras_model_path, validation_dir)
+        predict(keras_model, Path(args.predict_image_path), class_names)
 
     except Exception as e:
         print(f"Error: {e}")
